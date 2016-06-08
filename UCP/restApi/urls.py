614e5355ccb4930299df03d90f53f4146174d411
pranav <pranav15197@gmail.com>
2016-06-08 17:43:42 +0530

    Remove restApi app

--- UCP/restApi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from rest_framework import routers
-
-urlpatterns = [
-    url(r'^user/', include('login.user_urls')),
-    url(r'^password/', include('login.password_urls')),
-]
