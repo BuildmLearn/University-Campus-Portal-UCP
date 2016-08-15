6b20852b3b8de7fe0443714d7e7a1afcd7bb8a25
pranav <pranav15197@gmail.com>
2016-08-15 13:35:26 +0530

    Add event approval api

--- UCP/news_event/api.py
+++ UCP/news_event/api.py
@@ -17,6 +17,7 @@ from rest_framework import viewsets
 from news_event.models import News, Event
 from news_event.serializers import NewsSerializer, EventSerializer 
 
+from UCP.constants import result
 
 class NewsViewSet(mixins.ListModelMixin,
                 mixins.RetrieveModelMixin,
@@ -35,5 +36,32 @@ class EventViewSet(mixins.ListModelMixin,
     queryset = Event.objects.approved()
     serializer_class = EventSerializer
     
-    
+    @detail_route(methods=['post'])
+    def approve(self, request, pk):
+        response = {}
 
+        if Event.objects.pending().filter(id = pk).exists():
+            event = Event.objects.get(id = pk)
+            event.is_approved = True
+            event.save()
+
+            response["result"] = result.RESULT_SUCCESS
+        else:
+            response["result"] = result.RESULT_FAILURE
+            response["error"] = "This Event id does not exist"
+            
+    
+        return Response(response, status=status.HTTP_200_OK)
+    
+    
+    def get_queryset(self):
+        if self.action == "approve":
+            return Event.objects.all()
+        else:
+            return Event.objects.approved()
+            
+    def get_serializer_class(self):
+        if self.action == "approve":
+            return None
+        else:
+            return EventSerializer
