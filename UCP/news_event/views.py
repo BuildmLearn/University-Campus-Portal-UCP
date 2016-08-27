0ce791607ff00a26edfdbab68701be4621413db8
pranav <pranav15197@gmail.com>
2016-08-27 21:25:29 +0530

    Added documentation for news and events views

--- UCP/news_event/views.py
+++ UCP/news_event/views.py
@@ -1,3 +1,9 @@
+"""
+Views file for News and Events App
+
+contains views for the frontend pages of the News and Event App
+"""
+
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
@@ -11,32 +17,43 @@ from news_event.models import News, Event
 from news_event import functions
 from UCP.functions import get_base_context
 from UCP.settings import PAGE_SIZE
-# Create your views here.
 
 
 class NewsList(ListView):
+    """
+    Returns paginated list of all news articles
+    If tag is send as a query, then the list is filtered by tag
     
+    """
     model = News
     context_object_name = 'news_list'    
-    paginate_by = 10
+    paginate_by = PAGE_SIZE
 
     def get_context_data(self, **kwargs):
         context = super(NewsList, self).get_context_data(**kwargs)
+        
         context['user'] = get_base_context(self.request)["user"]
+        
+        #add the list of page numbers to be displayed in the pagination at the bottom of the page
         news_list = self.get_queryset()
         count = len(news_list)
         page_count = count/PAGE_SIZE + 1
-
+        context["pages"] = range(1, page_count+1)
+        
+        #if a tag is sent as a query, a link to subscribe to that tag is displayed at the top of the list
+        #if the user already follows that tag, this link is not shown
         my_tags = [tag.name for tag in context['user'].followed_tags.all()]
         
         if 'tag' in self.request.GET and not self.request.GET["tag"] in my_tags:
-            
+            #tag added to the context to show follow link
             context['tag'] = self.request.GET["tag"]
             
-        context["pages"] = range(1, page_count+1)
         return context
         
     def get_queryset(self):
+        """
+        returns list of news article, filtered by tag or page number or both
+        """
         if 'tag' in self.request.GET:
             news_list = News.objects.filter(tags__name = self.request.GET["tag"])
             count = len(news_list)
@@ -54,19 +71,24 @@ class NewsList(ListView):
 
 
 class NewsDetail(DetailView):
-    
+    """
+    view for details page for a news article
+    """
     model = News
     context_object_name = "news"
     
     def get_context_data(self, **kwargs):
         context = super(NewsDetail, self).get_context_data(**kwargs)
         context['user'] = get_base_context(self.request)["user"]
-        print context
         return context
         
 
 class EventList(ListView):
+    """
+    Returns paginated list of all events 
+    If tag is send as a query, then the list is filtered by tag
     
+    """
     model = Event
     context_object_name = 'events'    
     paginate_by = 10
@@ -104,7 +126,9 @@ class EventList(ListView):
 
 
 class EventDetail(DetailView):
-    
+    """
+    returns detail page for an event
+    """
     model = Event
     context_object_name = "event"
     
@@ -115,16 +139,25 @@ class EventDetail(DetailView):
         
         
 class EventCreate(View):
-    
+    """
+    handles creation of an event
+    """
     @method_decorator(login_required)
     def get(self, request):
+        """
+        returns the add event form
+        """
         context = get_base_context(request)
+        #tags used for the autocomplete feature of the tag input
         context["tags"] = get_all_tags()
 
         return render(request, 'news_event/add-event.html', context)
     
     @method_decorator(login_required)
     def post(self, request):
+        """
+        creates an event with the data send from the add event form
+        """
         context = get_base_context(request)
         response = functions.add_event(request)
         
@@ -132,21 +165,31 @@ class EventCreate(View):
             return redirect('/events/')
         else:
             context["tags"] = get_all_tags()
-            print response["error"]
+            #if creation of event failed return to add event form with the errors
             return render(request, 'news_event/add-event.html', context)
         
         
 class NewsCreate(View):
-    
+    """
+    Handles creation of news
+    accessible only to user with a moderator status
+    """
     @method_decorator(login_required)
     def get(self, request):
+        """
+        returns add news form
+        """
         context = get_base_context(request)
+        #tags used for the autocomplete feature of the tag input
         context["tags"] = get_all_tags()
 
         return render(request, 'news_event/add-news.html', context)
     
     @method_decorator(login_required)
     def post(self, request):
+        """
+        creates a news article with data from the add news form
+        """
         context = get_base_context(request)
         response = functions.add_news(request)
         
@@ -154,7 +197,7 @@ class NewsCreate(View):
             return redirect('/news/')
         else:
             context["tags"] = get_all_tags()
-            print response["error"]
+            #if creation of event failed return to add event form with the errors
             return render(request, 'news_event/add-news.html', context)
     
         
